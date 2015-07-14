c4501986659091f5337a6ca51cf69704c925aab7
Dave Long <dlong@cagedata.com>
2015-07-13 20:46:07 -0400

    Finish writing photobooth.

--- booth.py
+++ booth.py
@@ -1,20 +1,49 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
 import signal
 import sys
+import os
+import picamera
 
-leds = [7, 11, 13]
-button_pin = 15
+import interface
+
+leds = (8, 10, 9, 11)
+picture_button = 24
+quit_button = 23
+shutdown_button = 25
+reboot_button = 22
+
+_app_root = os.path.dirname(os.path.realpath(__file__))
+_pics_root = _app_root + "/pics"
 
 def main():
-	print("Running")
-	GPIO.setmode(GPIO.BOARD)
+        print "GPIO RPI Revision: ", GPIO.RPI_REVISION
+        print "GPIO Version: ", GPIO.VERSION
+        
+	print "Initializing"
+        if not os.path.exists(_pics_root):
+                os.makedirs(_pics_root)
+	GPIO.setmode(GPIO.BCM)
 	for led in leds:
 		GPIO.setup(led, GPIO.OUT)
-	GPIO.setup(button_pin, GPIO.IN)
+                GPIO.output(led, False)
+        for button in (picture_button, quit_button, shutdown_button, reboot_button):
+                GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 	# TODO: Setup handling to ensure no event queue
-	GPIO.add_event_detect(button_pin, GPIO.RISING, callback=take_picture, bouncetime=300)
+	# GPIO.add_event_detect(picture_button, GPIO.RISING, callback=take_picture, bouncetime=300)
+
+        GPIO.add_event_detect(quit_button, GPIO.RISING, callback=quit_handler, bouncetime=300)
+        GPIO.add_event_detect(shutdown_button, GPIO.RISING, callback=shutdown_handler, bouncetime=300)
+        GPIO.add_event_detect(reboot_button, GPIO.RISING, callback=reboot_handler, bouncetime=300)
+
+        reset()
+
+def reset():
+        interface.init_pygame()
+        interface.show_image("intro.png")
 
 def on_all():
 	for led in leds:
@@ -30,23 +59,71 @@ def on(pin):
 def off(pin):
 	GPIO.output(pin, GPIO.LOW)
 
-def take_picture(channel):
-	print("Taking a picture")
+def take_picture():
+	print "Taking a picture"
 	on_all()
+        interface.show_image("instructions.png")
 	for led in leds:
 		time.sleep(1)
 		off(led)
 	time.sleep(0.5)
 	on_all()
-	time.sleep(1)
+	# time.sleep(1)
+        # Take the picture
+        snap()
 	off_all()
+        time.sleep(5)
+        reset()
+
+def snap():
+        camera = picamera.PiCamera()
+        resolution = (interface.display_info.current_w, interface.display_info.current_h)
+        resolution = (1920, 1080)
+        camera.resolution = resolution
+        camera.start_preview()
+        time.sleep(1)
+        now = time.strftime("%Y-%m-%d-%H:%M:%S")
+        try:
+                GPIO.output(leds[0], GPIO.LOW)
+		for i, filename in enumerate(camera.capture_continuous(_pics_root + "/" + now + "-{counter}.jpg")):
+                        if i == (len(leds)-1):
+                                break
+                        time.sleep(2)
+                        GPIO.output(leds[i+1], GPIO.LOW)
+        finally:
+                interface.show_image("finished2.png")
+                camera.stop_preview()
+                camera.close()
+
+def quit():
+        print "Quitting Photobooth"
+        on(leds[0])
+        time.sleep(1)
+        GPIO.cleanup()
+        sys.exit(0)
+
+def quit_handler(channel):
+        quit()
+
+def shutdown_handler(channel):
+        print "Shutting down..."
+        on_all()
+        time.sleep(3)
+        os.system("shutdown -h now")
+
+def reboot_handler(channel):
+        print "Rebooting..."
+        on_all()
+        time.sleep(3)
+        os.system("reboot")
+        
 
 def signal_handler(signal, frame):
-	print("Shutting down")
-	GPIO.cleanup()
-	sys.exit(0)
+        quit()
 signal.signal(signal.SIGINT, signal_handler)
 
 main()
 while True:
-	time.sleep(0.5)
+        GPIO.wait_for_edge(picture_button, GPIO.RISING)
+	time.sleep(0.2)
+        take_picture()
--- interface.py
+++ interface.py
@@ -4,43 +4,43 @@ import pygame
 import os
 from time import sleep
 
-class Interface:
-    real_path = os.path.dirname(os.path.realpath(__file__))
+_real_path = os.path.dirname(os.path.realpath(__file__))
 
-    ###
-    # Initialize and configure a pygame instant
-    ###
-    def init_pygame(self):
+display_info = None
+
+def init_pygame():
         pygame.init()
-        self.display_info = pygame.display.Info()
-        size = (self.display_info.current_w, self.display_info.current_h)
+        global display_info
+        display_info = pygame.display.Info()
+        size = (display_info.current_w, display_info.current_h)
         pygame.display.set_caption("Photo Booth")
         pygame.mouse.set_visible(False)
         return pygame.display.set_mode(size, pygame.FULLSCREEN)
 
-    ###
-    # Display an image in the pygame instance
-    ###
-    def show_image(self, image):
-        screen = self.init_pygame()
-        img = pygame.image.load(self.real_path + "/media/" + image)
-        offset_x, offset_y = self.offsets(img.get_size())
+###
+# Display an image in the pygame instance
+###
+def show_image(image):
+        img = pygame.image.load(_real_path + "/media/" + image)
+        offset_x, offset_y = offsets(img.get_size())
         screen.blit(img, (offset_x, offset_y))
         pygame.display.flip()
 
-    ###
-    # Provide dynamic offsets to center on screen
-    ###
-    def offsets(self, dimensions):
-        offset_x = (self.display_info.current_w - dimensions[0]) / 2
-        offset_y = (self.display_info.current_h - dimensions[1]) / 2
+###
+# Provide dynamic offsets to center on screen
+###
+def offsets(dimensions):
+        offset_x = (display_info.current_w - dimensions[0]) / 2
+        offset_y = (display_info.current_h - dimensions[1]) / 2
         return (offset_x, offset_y)
 
-def main():
+screen = init_pygame()
+
+def _main():
     interface = Interface()
     for image in ["intro", "instructions", "processing", "finished2"]:
         interface.show_image(image + ".png")
         sleep(2)
 
 if __name__ == "__main__":
-    main()
+    _main()
